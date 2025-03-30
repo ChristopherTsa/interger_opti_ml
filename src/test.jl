c3e00cc665f7c7b0539fbc107297eb70c096c4f9
ChristopherTsa <christopher.tsa@ensta-paris.fr>
2025-03-30 23:50:46 +0200

    parallelized

--- src/test.jl
+++ src/test.jl
@@ -7,6 +7,7 @@ include("../src/main_iterative_algorithm_constrained.jl")
 include("../src/main_iterative_algorithm_exact.jl")
 include("../src/main_iterative_algorithm_lda.jl")
 include("../src/main_iterative_algorithm_simple.jl")
+using Base.Threads
 
 #main()
 
@@ -15,7 +16,15 @@ include("../src/main_iterative_algorithm_simple.jl")
 #main_merge_lda()
 #main_merge_simple()
 
-main_iterative_constrained()
-#main_iterative_exact()
-main_iterative_lda()
-main_iterative_simple()
+# Run the three functions in parallel using tasks
+task1 = @spawn main_iterative_constrained()
+#task_exact = @spawn main_iterative_exact()
+task2 = @spawn main_iterative_lda()
+task3 = @spawn main_iterative_simple()
+
+# Wait for all tasks to complete
+fetch(task1)
+fetch(task2)
+fetch(task3)
+
+println("All parallel tasks completed")
